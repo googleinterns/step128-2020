a31f5eafbc731c4d0cfc06f81b5406569dc0cec3
Nicole Sullivan <nicolesullivanmarie@gmail.com>
2020-06-22 16:50:12 +0000

    Add logger import and close doPost function

--- src/main/java/com/google/sps/servlets/DataServlet.java
+++ src/main/java/com/google/sps/servlets/DataServlet.java
@@ -19,6 +19,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.logging.Logger;
 
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
@@ -32,5 +33,6 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
+    
+  }
 }
