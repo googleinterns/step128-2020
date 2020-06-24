467cf03a7d71ee38401182b32e7faff7e0a185ac
Helen Fang <helenfang524@gmail.com>
2020-06-24 20:23:01 +0000

    Added selection toggle for each survey question

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -299,3 +299,15 @@ async function getEvents(url, index, option) {
     });
   });
 }
+
+var surveyResponses = [-1, -1, -1, -1, -1];
+function toggleSurveyDisplay(question, index) {
+	const circle = document.getElementsByClassName('survey-select')[question*5 + index];
+	if(surveyResponses[question] >= 0) {
+		var oldIndex = surveyResponses[question];
+		const oldCircle = document.getElementsByClassName("survey-select")[question*5 + oldIndex];
+		oldCircle.style.backgroundColor = "transparent";
+	}
+	circle.style.backgroundColor = "white";
+	surveyResponses[question] = index;
+}
