c6581bdca3f8b0699adc3fcd9e94b88f7f5b29fb
Helen Fang <helenfang524@gmail.com>
2020-06-26 21:58:43 +0000

    Delete unimplemented method

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -12,11 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/* pick the stylesheet and nav bar */
-function loadActions() {
-
-}
-
 function generateNavBar() {
   const headerLeft = document.createElement('div');
   headerLeft.className = 'header-left';
