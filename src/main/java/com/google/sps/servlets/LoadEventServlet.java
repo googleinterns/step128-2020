5590bf77913b360a6da40cf8945168b7ee2e5235
Nicole Sullivan <nicolesullivanmarie@gmail.com>
2020-07-01 19:42:50 +0000

    Remove unused import

--- src/main/java/com/google/sps/servlets/LoadEventServlet.java
+++ src/main/java/com/google/sps/servlets/LoadEventServlet.java
@@ -30,7 +30,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
-import com.google.gson.Gson;
 
 @WebServlet("/load-event")
 public class LoadEventServlet extends HttpServlet {
