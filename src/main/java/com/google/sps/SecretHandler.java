8024b6324e1f1ff17fee3ca672f0f18ac0a2b1ba
William <williamlew@google.com>
2020-07-13 17:07:08 +0000

    Add secret for Firebase Admin key

--- src/main/java/com/google/sps/SecretHandler.java
+++ src/main/java/com/google/sps/SecretHandler.java
@@ -14,14 +14,18 @@
 
 package com.google.sps;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.secretmanager.v1.AccessSecretVersionResponse;
 import com.google.cloud.secretmanager.v1.SecretManagerServiceClient;
 import com.google.cloud.secretmanager.v1.SecretVersionName;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class SecretHandler {
   private static final String PROJECT_ID = "unitebystep";
   private static final String API_KEY_ID = "api-key";
+  private static final String FIREBASE_KEY_ID = "firebase-key";
   private static final String VERSION_ID = "1";
 
   /**
@@ -47,5 +51,32 @@ public class SecretHandler {
       }
       return result;
     }
+  }
+
+  /**
+   * Returns a GoogleCredentials object containing the Firebase Admin key. Throws an exception if
+   * the Secret manager client is unable to be created.
+   */
+  public static GoogleCredentials getFirebaseKey() throws IOException {
+    SecretManagerServiceClient client = null;
+    String result = "";
+    GoogleCredentials cred = null;
+    try {
+      client = SecretManagerServiceClient.create();
+      SecretVersionName secretVersionName =
+          SecretVersionName.of(PROJECT_ID, FIREBASE_KEY_ID, VERSION_ID);
+
+      AccessSecretVersionResponse response = client.accessSecretVersion(secretVersionName);
+      result = response.getPayload().getData().toStringUtf8();
+    } catch (IOException e) {
+      throw new IOException("failed to create secret manager service client");
+    } finally {
+      if (client != null) {
+        client.close();
+      }
+    }
+    InputStream stream = new ByteArrayInputStream(result.getBytes());
+    cred = GoogleCredentials.fromStream(stream);
+    return cred;
   }
 }
