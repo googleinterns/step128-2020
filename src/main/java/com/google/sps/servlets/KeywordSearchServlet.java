bc7cedac957c29396b75a435f3442dd4664fc7b4
William <williamlew@google.com>
2020-07-17 23:23:23 +0000

    Split SearchServlet keyword functionality into separated KeywordSearchServlet

--- src/main/java/com/google/sps/servlets/KeywordSearchServlet.java
+++ src/main/java/com/google/sps/servlets/KeywordSearchServlet.java
@@ -30,17 +30,27 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/search")
-public class SearchServlet extends HttpServlet {
+@WebServlet("/ksearch")
+public class KeywordSearchServlet extends HttpServlet {
 
-  private static final Logger LOGGER = Logger.getLogger(SearchServlet.class.getName());
+  private static final Logger LOGGER = Logger.getLogger(KeywordSearchServlet.class.getName());
+  // Number of keywords to produce
+  private static final int NUM_KEYWORDS = 5;
+  // Minimum number of instances to be considered a keyword
+  private static final int MIN_INSTANCES = 2;
+  // List of words to ignore
+  // This list is incomplete
+  private static final List<String> IRRELEVANT_WORDS =
+      new ArrayList<String>(Arrays.asList("the", "is", "for", "in", "of", "so", "to"));
   // Conversion rate from mi to km
   private static final double MI_TO_KM = 1.609;
 
@@ -146,5 +156,94 @@ public class SearchServlet extends HttpServlet {
     List<String> tagListC = new ArrayList<String>(tagListA);
     tagListC.retainAll(tagListB);
     return ((double) tagListC.size()) / tagListA.size();
+  }
+
+  /**
+   * Returns keywords from an event (currently using just the title and description) based off their
+   * frequency and appearance in the title vs in the description.
+   *
+   * @param title String representing the title text to be processed
+   * @param desc String representing the description text to be processed
+   * @return List containing most important words from the string
+   */
+  public static List<String> getKeywords(String title, String desc) {
+    // TODO: convert to lowercase in processing (figure out acronyms)
+    Map<String, Integer> titleMap = KeywordSearchServlet.wordCount(title);
+    Map<String, Integer> descMap = KeywordSearchServlet.wordCount(desc);
+
+    // Merge maps
+    // Title occurrence values multiplied by 2 to give more weight than description
+    titleMap.forEach((key, value) -> descMap.merge(key, value * 2, (v1, v2) -> v1 + (v2 * 2)));
+    List<Map.Entry<String, Integer>> mergeList =
+        new ArrayList<Map.Entry<String, Integer>>(descMap.entrySet());
+
+    Collections.sort(
+        mergeList,
+        new Comparator<Map.Entry<String, Integer>>() {
+          public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+            return (o2.getValue()).compareTo(o1.getValue());
+          }
+        });
+
+    // Add top results to the final list
+    List<String> finalList = new ArrayList<String>();
+    int count = 0;
+    while (finalList.size() < NUM_KEYWORDS) {
+      Map.Entry e = mergeList.get(count);
+      // Exclude words with less appearances than the cutoff
+      if (((int) e.getValue()) < MIN_INSTANCES) break;
+      else if (count >= mergeList.size()) break;
+      // Exclude common useless words (in, a, the, etc)
+      else if (!IRRELEVANT_WORDS.contains(e.getKey().toString())) {
+        finalList.add(e.getKey().toString());
+      }
+      count++;
+    }
+
+    return finalList;
+  }
+
+  /**
+   * Returns a list of words contained in a string.
+   *
+   * @param str String to be processed
+   * @return List containing every word in a string
+   */
+  public static List<String> getSeparateWords(String str) {
+    // TODO: handle single quoted strings (right now keeps them in for
+    // the common case of contractions
+
+    // Split the string based on a regex: splits at every space and at
+    // most common non-alphabet characters
+    String[] list = str.split("([\\s.,!?:;()\\[\\]&\"\\s+])");
+    // Trim all whitespace
+    for (int i = 0; i < list.length; i++) {
+      list[i] = list[i].replaceAll("\\s+", "");
+    }
+    List<String> listArr = new ArrayList<String>(Arrays.asList(list));
+    // Remove all empty strings
+    listArr.removeAll(Arrays.asList("", null));
+    return listArr;
+  }
+
+  /**
+   * Returns a map of the occurrences of words in a string.
+   *
+   * @param input String to be processed
+   * @return Map containing word keys and instance count values
+   */
+  public static Map<String, Integer> wordCount(String input) {
+    List<String> words = KeywordSearchServlet.getSeparateWords(input);
+    // process title words
+    Map<String, Integer> map = new HashMap<String, Integer>();
+    for (String w : words) {
+      w = w.toLowerCase();
+      if (map.containsKey(w)) {
+        map.put(w, map.get(w) + 1);
+      } else {
+        map.put(w, 1);
+      }
+    }
+    return map;
   }
 }
---
+++ src/test/java/com/google/sps/KeywordSearchServletTest.java
@@ -0,0 +1,166 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.google.maps.DistanceMatrixApi;
+import com.google.maps.DistanceMatrixApiRequest;
+import com.google.maps.GeocodingApi;
+import com.google.maps.GeocodingApiRequest;
+import com.google.sps.servlets.KeywordSearchServlet;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@PowerMockIgnore("okhttp3.*")
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({
+  DistanceMatrixApi.class,
+  DistanceMatrixApiRequest.class,
+  GeocodingApi.class,
+  GeocodingApiRequest.class
+})
+public final class KeywordSearchServletTest {
+  private final LocalServiceTestHelper helper =
+      new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+  private KeywordSearchServlet testKeywordSearchServlet;
+
+  @Before
+  public void setUp() {
+    helper.setUp();
+    testKeywordSearchServlet = new KeywordSearchServlet();
+  }
+
+  @After
+  public void tearDown() {
+    helper.tearDown();
+  }
+
+  @Test
+  public void getsSeparateWordsCorrectly() throws IOException {
+    String text = "The quick brown fox jumps over the lazy dog.";
+    List<String> correctList =
+        new ArrayList<String>(
+            Arrays.asList("The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"));
+    List<String> testList = KeywordSearchServlet.getSeparateWords(text);
+    assertEquals(correctList, testList);
+  }
+
+  @Test
+  public void handlesAllSeparatorsCorrectly() throws IOException {
+    String text = "Are you sure? I... well; I'm not:quite, " + "personally(hehe)[okay]";
+    List<String> correctList =
+        new ArrayList<String>(
+            Arrays.asList(
+                "Are",
+                "you",
+                "sure",
+                "I",
+                "well",
+                "I'm",
+                "not",
+                "quite",
+                "personally",
+                "hehe",
+                "okay"));
+    List<String> testList = KeywordSearchServlet.getSeparateWords(text);
+    assertEquals(correctList, testList);
+  }
+
+  @Test
+  public void getsTitleAndDescKeywordsCorrectly() throws IOException {
+    String titleText =
+        "Climate climate climate climate Protest! Climate " + "protest: Protest protest!";
+    String descText =
+        "Important! Please come out in support of the environment. The "
+            + "environment is so important to us! Protect the environment. Come!";
+    // climate - 5, w/ weight 10
+    // protest - 4, w/ weight 8
+    // environment - 3
+    // come - 2
+    // important - 2
+    List<String> correctList =
+        new ArrayList<String>(
+            Arrays.asList("climate", "protest", "environment", "come", "important"));
+    List<String> testList = KeywordSearchServlet.getKeywords(titleText, descText);
+    assertEquals(correctList, testList);
+  }
+
+  @Test
+  public void getsTitleAndDescKeywordsCorrectlyWeighted() throws IOException {
+    String titleText = "Climate! Climate change protest: Protest for our children!";
+    String descText =
+        "Important! Please come out in support of the environment. The "
+            + "environment is so important important to us! Protect the environment. Come come!";
+    // climate - 4, w/o weight 2
+    // protest - 4, w/o weight 2
+    // environment - 3
+    // come - 3
+    // important - 3
+    List<String> correctList =
+        new ArrayList<String>(
+            Arrays.asList("protest", "climate", "come", "important", "environment"));
+    List<String> testList = KeywordSearchServlet.getKeywords(titleText, descText);
+    assertEquals(correctList, testList);
+  }
+
+  @Test
+  public void keywordListsMergeCorrectly() throws IOException {
+    String titleText = "Climate change protest";
+    String descText = "Protest protest in support of the environment environment environment.";
+    // protest - 4
+    // environment - 3
+    // change - 2
+    // climate - 2
+    // support - 1, so its cut off
+    List<String> correctList =
+        new ArrayList<String>(Arrays.asList("protest", "environment", "change", "climate"));
+    List<String> testList = KeywordSearchServlet.getKeywords(titleText, descText);
+    assertEquals(correctList, testList);
+  }
+
+  @Test
+  public void getKeywordsHasMax5() throws IOException {
+    String titleText = "";
+    String descText =
+        "Climate climate climate climate change change change"
+            + " protest protest protest for Earth Earth. Important movement "
+            + "movement to movement movement movement important important "
+            + "important important important.";
+    // important - 6
+    // movement - 5
+    // climate - 4
+    // change - 3
+    // protest - 3
+    // earth should be cut off
+    List<String> correctList =
+        new ArrayList<String>(
+            Arrays.asList("important", "movement", "climate", "change", "protest"));
+    List<String> testList = KeywordSearchServlet.getKeywords(titleText, descText);
+    assertEquals(correctList, testList);
+  }
+}
