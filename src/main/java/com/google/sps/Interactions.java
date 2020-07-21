f524bba4c0d8ff0f02ed5c9be3a3125683b84897
Helen Fang <helenfang524@gmail.com>
2020-07-21 20:42:35 +0000

    Add utility methods for user preference and saving interactions

--- src/main/java/com/google/sps/Interactions.java
+++ src/main/java/com/google/sps/Interactions.java
@@ -20,7 +20,15 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.PreparedQuery.TooManyResultsException;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
 
@@ -32,6 +40,10 @@ public class Interactions {
   public static final int VIEW_SCORE = 4;
   public static final int SAVE_SCORE = 8;
   public static final int CREATE_SCORE = 10;
+
+  // affects on scores of "undoing" actions
+  public static final int UNSAVE_DELTA = -2;
+  public static final int DELETE_DELTA = -3;
 
   // interest categories, matches question order on survey pages
   public static final String[] metrics = {"environment", "blm", "volunteer", "education", "LGBTQ+"};
@@ -92,5 +104,64 @@ public class Interactions {
       }
     }
     return result;
+  }
+
+  /**
+   * Creates an Interaction entity in datastore recording an interaction between user and event.
+   *
+   * @return change of user's rating on an item (saves highest score only)
+   */
+  public static int recordInteraction(String userId, long eventId, int score) {
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Query q =
+        new Query("Interaction")
+            .setFilter(
+                CompositeFilterOperator.and(
+                    new FilterPredicate("user", FilterOperator.EQUAL, userId),
+                    new FilterPredicate("event", FilterOperator.EQUAL, eventId)));
+    PreparedQuery pq = datastore.prepare(q);
+    Entity interactionEntity = null;
+    int delta = score;
+    try {
+      interactionEntity = pq.asSingleEntity();
+      int prevScore = Integer.parseInt(interactionEntity.getProperty("rating").toString());
+      if (prevScore < score) {
+        interactionEntity.setProperty("rating", score);
+      }
+      delta = score - prevScore;
+    } catch (TooManyResultsException e) {
+      // delete existing entries and re-create later
+      List<Key> toDelete = new ArrayList<>();
+      for (Entity entity : pq.asIterable()) {
+        toDelete.add(entity.getKey());
+      }
+      datastore.delete(toDelete);
+    } catch (NullPointerException e) {
+      // do nothing, pass to finally block
+    } finally {
+      if (interactionEntity == null) {
+        interactionEntity = new Entity("Interaction");
+        interactionEntity.setProperty("user", userId);
+        interactionEntity.setProperty("event", eventId);
+        interactionEntity.setProperty("rating", score);
+      }
+      interactionEntity.setProperty("timestamp", System.currentTimeMillis());
+      datastore.put(interactionEntity);
+    }
+    return delta;
+  }
+
+  /** Updates user preference map. */
+  public static void updatePrefs(Entity userEntity, List<String> tags, int score) {
+    if (!userEntity.getKind().equals("User")) {
+      throw new IllegalArgumentException("must be user item");
+    }
+    for (String s : tags) {
+      if (userEntity.hasProperty(s)) {
+        userEntity.setProperty(s, score + Integer.parseInt(userEntity.getProperty(s).toString()));
+      } else {
+        userEntity.setProperty(s, score);
+      }
+    }
   }
 }
