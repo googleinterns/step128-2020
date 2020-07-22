2b6ac2014de8e00aadf7d5dce3a9bc890e30df5e
Helen Fang <helenfang524@gmail.com>
2020-07-22 21:15:07 +0000

    Get from datastore and make model

--- src/main/java/com/google/sps/Recommend.java
+++ src/main/java/com/google/sps/Recommend.java
@@ -3,9 +3,14 @@ package com.google.sps;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.Query;
 import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import org.apache.spark.ml.evaluation.RegressionEvaluator;
 import org.apache.spark.ml.recommendation.ALS;
@@ -15,6 +20,8 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
 public class Recommend {
+
+  private static final int UPPER_LIMIT = 5_000_000;
 
   private static SparkSession spark;
   private static DatastoreService datastore;
@@ -32,15 +39,33 @@ public class Recommend {
   }
 
   /** Rebuilds recommendation model and calculates recommendations for users. */
-  public static void calculateRecommend() {
+  public static void calculateRecommend() throws IOException {
     if (spark == null || datastore == null) {
       init();
     }
     Iterable<Entity> queriedUsers = datastore.prepare(new Query("User")).asIterable();
     // get user prefs
+    Map<Integer, String> userIdHash = new HashMap<>();
     Map<String, Map<String, Integer>> userPrefs = new HashMap<>();
     for (Entity e : queriedUsers) {
-      //   userPrefs.put(e.getKey().getName(), Interactions.buildVectorForEvent(e));
+      String id = e.getKey().getName();
+      userPrefs.put(id, Interactions.buildVectorForEntity(e));
+      userIdHash.put(id.hashCode(), id);
+    }
+    Map<Integer, Long> eventIdHash = new HashMap<>();
+    Dataset<Row> ratings = makeRatingsDataframe(eventIdHash);
+    ALSModel model = trainModel(null, ratings);
+
+    Dataset<Row> userRecs = model.recommendForAllUsers(150);
+    List<Row> userRecsList = userRecs.collectAsList();
+    for (Row recRow : userRecsList) {
+      int hash = recRow.getInt(0); // hashcode of user id
+      List<Row> predScores = recRow.getList(1); // id-score pairs for items
+
+      for (Row itemRow : predScores) {
+        int itemHash = itemRow.getInt(0); // hashcode of event id
+        float predScore = itemRow.getFloat(1); // pred score
+      }
     }
 
     Iterable<Entity> currentRecs = datastore.prepare(new Query("UserRecs")).asIterable();
@@ -53,9 +78,6 @@ public class Recommend {
    * @param training Will fit the model to this training dataset
    */
   public static ALSModel trainModel(String path, Dataset<Row> training) throws IOException {
-    if (spark == null) {
-      init();
-    }
     ALS als =
         new ALS().setMaxIter(5).setUserCol("userId").setItemCol("eventId").setRatingCol("rating");
     ALSModel model = als.fit(training);
@@ -68,11 +90,6 @@ public class Recommend {
     return model;
   }
 
-  /** Returns an existing ALSModel. */
-  public static ALSModel getModel(String path) {
-    return ALSModel.load(path);
-  }
-
   /** Uses RMSE to evaluate a set of predicted data. */
   public static double evaluatePredictions(Dataset<Row> predictions) {
     RegressionEvaluator evaluator =
@@ -81,5 +98,99 @@ public class Recommend {
             .setLabelCol("rating")
             .setPredictionCol("prediction");
     return evaluator.evaluate(predictions);
+  }
+
+  /**
+   * Constructs ratings dataframe. Also keeps track of event ids with their hashcodes and deletes
+   * oldest Interaction entries from datastore.
+   */
+  private static Dataset<Row> makeRatingsDataframe(Map<Integer, Long> eventIdHash) {
+    Iterable<Entity> queriedInteractions =
+        datastore
+            .prepare(new Query("Interaction").addSort("timestamp", Query.SortDirection.DESCENDING))
+            .asIterable();
+    Iterator<Entity> itr = queriedInteractions.iterator();
+    int count = 0;
+    // convert Entities to spark-friendly format
+    List<EventRating> ratings = new ArrayList<>();
+    // keep track of eventId hashcodes and eventIds
+    while (itr.hasNext() && count < UPPER_LIMIT) {
+      Entity entity = itr.next();
+      EventRating rate = EventRating.parseEntity(entity);
+      if (rate != null) {
+        count++;
+        ratings.add(rate);
+        long eventId = Long.parseLong(entity.getProperty("event").toString());
+        eventIdHash.put(rate.getEventId(), eventId);
+      }
+    }
+    // delete the oldest Interaction entries from datastore
+    List<Key> toDelete = new ArrayList<>();
+    while (itr.hasNext()) {
+      toDelete.add(itr.next().getKey());
+    }
+    datastore.delete(toDelete);
+    return spark.createDataFrame(ratings, EventRating.class);
+  }
+
+  public static class EventRating implements Serializable {
+    private int userId;
+    private int eventId;
+    private float rating;
+    private long timestamp;
+
+    public EventRating() {}
+
+    public EventRating(String userId, long eventId, float rating, long timestamp) {
+      // all fields must be numeric
+      this.userId = userId.hashCode();
+      // spark implicitly casts long to int
+      this.eventId = (Long.toString(eventId)).hashCode();
+      this.rating = rating;
+      this.timestamp = timestamp;
+    }
+
+    // need getters to work with spark.createDataFrame()
+    public int getUserId() {
+      return userId;
+    }
+
+    public int getEventId() {
+      return eventId;
+    }
+
+    public float getRating() {
+      return rating;
+    }
+
+    public long getTimestamp() {
+      return timestamp;
+    }
+
+    public static EventRating parseEntity(Entity entity) {
+      if (!entity.getKind().equals("Interaction")) {
+        return null;
+      }
+      Object userField = entity.getProperty("user");
+      Object eventField = entity.getProperty("event");
+      Object ratingField = entity.getProperty("rating");
+      Object timeField = entity.getProperty("timestamp");
+      if (userField == null || eventField == null || ratingField == null || timeField == null) {
+        return null;
+      }
+      try {
+        String userId = userField.toString();
+        long eventId = Long.parseLong(eventField.toString());
+        float rating = Float.parseFloat(ratingField.toString());
+        long timestamp = Long.parseLong(timeField.toString());
+        return new EventRating(userId, eventId, rating, timestamp);
+      } catch (NumberFormatException e) {
+        return null;
+      }
+    }
+
+    public String toString() {
+      return userId + " " + eventId + " " + rating + " " + timestamp;
+    }
   }
 }
