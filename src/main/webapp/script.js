d4171541a472f9cbe2556e9af596c6bc45ed31be
William <williamlew@google.com>
2020-07-23 19:45:02 +0000

    Add functionality to set a default location

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -758,6 +758,17 @@ async function getLocation() {
 }
 
 /**
+ * Checks if the user has a current location and if not sets it to the default
+ */
+async function checkLocation() {
+  const location = await getLocation();
+  if (location == '') {
+    setCookie('location', '90001');
+  }
+  return Promise.resolve();
+}
+
+/**
  * Prompts the user to enter their zipcode to change their location
  */
 function changeLocation() {
@@ -984,9 +995,11 @@ async function unsaveEvent(eventId) {
  * Load all actions for the search page.
  */
 function searchLoadActions() {
-  updateSearchBar();
-  updateTagBox();
-  getSearchDistanceSettings();
+  checkLocation().then(() => {
+    updateSearchBar();
+    updateTagBox();
+    getSearchDistanceSettings();
+  });
 }
 
 /**
